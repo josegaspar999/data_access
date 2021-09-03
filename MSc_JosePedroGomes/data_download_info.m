856535e3e5cf7c9778d8f642b4e9bb28f563035d
Jose Gaspar <josegaspar999@gmail.com>
2021-09-03 12:38:02 +0100

    demo folder

--- MSc_JosePedroGomes/data_download_info.m
+++ MSc_JosePedroGomes/data_download_info.m
@@ -4,32 +4,38 @@ datasets= {...
     struct('dataId','vrml',         'ofname',	'190600_vrml_chessboard.zip',       'url', 'https://www.dropbox.com/s/z1tnul4hdfe0rej/190600_vrml_chessboard.zip?dl=1'), ...
     ... %struct('dataId','imdata1', 'ofname', 'im_V1_02_medium.zip',	'url', 'http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/vicon_room1/V1_02_medium/V1_02_medium.zip'), ...
     ... %struct('dataId','evdata1', 'ofname', 'ev_shapes_rotation.zip', 'url', 'http://rpg.ifi.uzh.ch/datasets/davis/shapes_rotation.zip'), ...
-    mkData1, ...
-    mkData1x, ...
+    mkData1( 'imdata1'  ), ...
+    mkData1( 'imdata1x' ), ...
     mkData2( 'evdata1', 'shapes_rotation' ), ...
     mkData2( 'evdata2', 'boxes_rotation' ), ...
    };
 return
 
 
-function ret= mkData1
-dataId= 'imdata1';
-urlPath= 'http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/vicon_room1/V1_02_medium/';
-urlFilename= 'V1_02_medium';
-urlFileext= '.zip';
-localPathPrefix= 'im_';
-ret= mkDataInfo( dataId, urlPath, urlFilename, urlFileext, localPathPrefix );
-return
+function ret= mkData1( dataId )
+mkfolder= '';
 
+switch dataId
+    case 'imdata1'
+        urlPath= 'http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/vicon_room1/V1_02_medium/';
+        urlFilename= 'V1_02_medium';
+        urlFileext= '.zip';
+        localPathPrefix= 'im_';
+    case 'imdata1x'
+        urlPath= 'http://www.isr.tecnico.ulisboa.pt/~jag/data/vi_odometry/';
+        urlFilename= 'im_V1_02_medium_extra';
+        urlFileext= '.zip';
+        localPathPrefix= '';
+        mkfolder= 'im_V1_02_medium/mav0/cam0/data_extra';
+    otherwise
+        error('inv dataId')
+end
 
-function ret= mkData1x
-dataId= 'imdata1x';
-urlPath= 'http://www.isr.tecnico.ulisboa.pt/~jag/data/vi_odometry/';
-urlFilename= 'im_V1_02_medium_extra';
-urlFileext= '.zip';
-localPathPrefix= '';
 ret= mkDataInfo( dataId, urlPath, urlFilename, urlFileext, localPathPrefix );
-ret.mkfolder= 'im_V1_02_medium/mav0/cam0/data_extra';
+
+if ~isempty(mkfolder)
+    ret.mkfolder= mkfolder;
+end
 return
 
 
