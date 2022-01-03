10c2e5be5ff556f500e523577e040bc951c5744d
PAndreNog <62256197+PAndreNog@users.noreply.github.com>
2022-01-03 18:50:13 +0000

    Add files via upload
    Updated with final data folder

--- MSc_AndreNogueira/_readme.txt
+++ MSc_AndreNogueira/_readme.txt
@@ -1,22 +1,29 @@
 
-This folder contains datasets to use
-and is usually located in	c:\msc\GIT\data
+This folder contains
+	datasets to use
+	calibration data: cam (calib_640_v2.mat), imu (imu_params.mat), imu-cam (imu_img_trans.mat)
 
+and is usually located in
+	c:\msc\GIT\data
 
--- Usage:
+---
 
-Do once to get data into your computer:
+Typical usage, do once:
 datasets are stored elsewhere (online) and downloaded with:
 >> data_download
+
 where "data_download.m" reads information by running .\data_download_info.m
 
-Do everytime your code needs data:
->> ret= netcam3_data
-or choosing a specific dataset by specifying a dataId
->> ret= netcam3_data( dataId )
 
+For a demo where you show, as many times as desired, the dataset
+run the command / follow the next steps:
 
--- Datasets & simulation SW:
+>> neurocams3_data_tst
+
+---
+
+Datasets & simulation SW:
 
 EUROC MAV dataset V1_01_medium
 https://projects.asl.ethz.ch/datasets/doku.php?id=kmavvisualinertialdatasets
+https://www.dropbox.com/sh/o16tecm7n4bfkk0/AABcj3jCY-peCCfa8BQwIHrKa?dl=0
--- MSc_AndreNogueira/neurocams3_data_tst.m
+++ MSc_AndreNogueira/neurocams3_data_tst.m
@@ -1,15 +1,15 @@
 function neurocams3_data_tst( tstId )
 if nargin<1
-    tstId= 80; %60; %50; %41; %40; %32; %30;
+    tstId= 40; %60; %50; %41; %40; %32; %30;
 end
 
 switch tstId
     case {30, 31}
         figure(30); clf
         if tstId==30
-            ret= neurocams3_data( 'ims1_cam0' );
+            ret= neurocams3_data( 'imv1m_cam0' );
         else
-            ret= neurocams3_data( 'ims1_cam1' );
+            ret= neurocams3_data( 'imv1m_cam1' );
         end
         imax= length(ret.iRange);
         for i= 1:imax
@@ -22,8 +22,8 @@ switch tstId
 
     case 32
         figure(30); clf
-        ret1= neurocams3_data( 'ims1_cam0' );
-        ret2= neurocams3_data( 'ims1_cam1' );
+        ret1= neurocams3_data( 'imv1m_cam0' );
+        ret2= neurocams3_data( 'imv1m_cam1' );
         imax= length(ret1.iRange);
         for i= 1:imax
             f1= sprintf(ret1.bfname, ret1.iRange(i));
@@ -36,97 +36,66 @@ switch tstId
             drawnow
             if aborttst, break; end
         end
-
+        
     case 40
-        figure(40); clf
-        ret= neurocams3_data( 'ev_shapes_rotation' );
-        imax= max(ret.iRange);
-        for i= ret.iRange
-            fname= sprintf(ret.bfname, i);
-            imshow( fname )
-            title( sprintf('img %i (max %d)', i, imax) )
+        % Set up the figure and dataset
+        [~,f1,f2]=set_double_fig;
+        ret= neurocams3_data( 'mfpf_02' );
+        imax= length(ret.iRange);
+        
+        % Set up the orientation plot
+        tp = theaterPlot('Parent',f2,'XLimit',[-2 2],'YLimit',[-2 2],'ZLimit',[-2 2]);
+        op = orientationPlotter(tp,'DisplayName','Robot Orientation',...
+            'LocalAxesLength',2);
+        view(f2,[-110,38]);
+        title(f2, 'Robot Orientation');
+        
+        % Get the dataset images and IMU data
+        Images = readtable( neurocams3_data([], '../img.txt') );
+        tImages = table2array(Images(:,1));
+        fileImages = table2array(Images(:,2));
+        
+        IMU = load( neurocams3_data( [], '../imu.mat') );
+        t = IMU.time_float(:,1);
+        quat = IMU.quat';
+        quat0 = quat(1,:);
+        imumax = IMU.ii;
+        
+        for i= 1:imax
+            
+            imshow( [ret.pname, fileImages{i}], 'Parent',f1)
+            title( f1,sprintf('img %i of %d', i, imax) )
+            
+            idx = find( t > tImages(i), 1 );        
+            
+            if ~isempty(idx)
+                plotOrientation(op,...
+                    quaternion(quatmultiply(quatinv(quat0),quat(idx,:))) );
+                title( f2,sprintf('imu orientation %i of %d', idx, imumax) )
+            end
+            
             drawnow
             if aborttst, break; end
         end
         
-    case 41
-        pname= neurocams3_data( 'ev_shapes_rotation', struct('retJustPath',1) );
-        fprintf(1, 'Main folder: %s\n', pname);
+        
+        
+        
+end
 
-    case 42
-        dataId= 'ev_shapes_rotation';
-        ret= neurocams3_data( dataId );
+end
 
-        % example of one known filename
-        fname= neurocams3_data( dataId, 'events.txt' );
-        fprintf(1, '0: %s\n', fname);
 
-        % example of *.txt
-        d= dir( [ret.pname '*.txt'] );
-        for i= 1:length(d)
-            fname= neurocams3_data( dataId, d(i).name );
-            fprintf(1, '%d: %s\n', i, fname);
-        end
-        
+function [fall, f1, f2] = set_double_fig
+    fall = figure(99);
+    oldUnits = get( fall, 'Units' );
+    set( fall, 'Units', 'normalized' );
+    set( fall, 'Position', [0.01,0.2,0.7,0.4] );
+    set( fall, 'Units', oldUnits );
 
-        
-    case 50
-        figure(50); clf
-        ret= neurocams3_data( 'ev_boxes_rotation' );
-        imax= max(ret.iRange);
-        for i= ret.iRange
-            fname= sprintf(ret.bfname, i);
-            imshow( fname )
-            title( sprintf('img %i (max %d)', i, imax) )
-            drawnow
-            if aborttst, break; end
-        end
-        
-    case 60
-        figure(60); clf
-        ret= neurocams3_data( 'ev_translation_1_x' );
-        imax= max(ret.iRange);
-        for i= ret.iRange
-            fname= sprintf(ret.bfname, i);
-            imshow( fname )
-            title( sprintf('img %i (max %d)', i, imax) )
-            drawnow
-            if aborttst, break; end
-        end
-        
-    case 70
-        figure(70); clf
-        ret= neurocams3_data( 'ev_rotation_18_y' );
-        imax= max(ret.iRange);
-        for i= ret.iRange
-            fname= sprintf(ret.bfname, i);
-            imshow( fname )
-            title( sprintf('img %i (max %d)', i, imax) )
-            drawnow
-            if aborttst, break; end
-        end
-        
-    case 80
-        figure(70); clf
-        ret= neurocams3_data( 'ev_rotation_18_x' );
-        imax= max(ret.iRange);
-        for i= ret.iRange
-            fname= sprintf(ret.bfname, i);
-            imshow( fname )
-            title( sprintf('img %i (max %d)', i, imax) )
-            drawnow
-            if aborttst, break; end
-        end
-        
-    case 90
-        figure(70); clf
-        ret= neurocams3_data( 'ev_rotation_18_z' );
-        imax= max(ret.iRange);
-        for i= ret.iRange
-            fname= sprintf(ret.bfname, i);
-            imshow( fname )
-            title( sprintf('img %i (max %d)', i, imax) )
-            drawnow
-            if aborttst, break; end
-        end
+    % models_fig = subplot('position',[0.05 0.1 0.05 0.8]);
+    f1 = subplot('position',[0.05 0.1 0.425 0.8]);
+    f2 = subplot('position',[0.575 0.1 0.375 0.8]);
+    
+    view(-180,0);
 end
